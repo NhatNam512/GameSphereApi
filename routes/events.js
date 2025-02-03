1aeaf4a08481bf77b4a8a75d7087b4981131a5bf
Nguyễn Nhật Nam <nhatnam5122004@gmail.com>
2025-02-03 11:20:14 +0700

    refactor event routes; improve error handling and add detail retrieval endpoint

--- routes/events.js
+++ routes/events.js
@@ -5,31 +5,79 @@ const config = require("../until/tokenConfig");
 const eventModel = require('../models/eventModel');
 
 router.get("/all", async function (req, res) {
-  try{
-  const events = await eventModel.find();
-  res.status(200).json({
-    status: true,
-    message: "Lấy danh sách sự kiện thành công",
-    data: events 
-  });
-  }catch(e){
-    res.status(400).json({ status: false, message: "Error" + e});
+  try {
+    const events = await eventModel.find();
+    res.status(200).json({
+      status: true,
+      message: "Lấy danh sách sự kiện thành công",
+      data: events
+    });
+  } catch (e) {
+    res.status(400).json({ status: false, message: "Error" + e });
+  }
+});
+
+router.get("/detail:id", async function (req, res) {
+  try {
+    const { id } = req.params;
+    var detail = await productModel.findById(id);
+
+    if (detail) {
+      res.status(200).json(detail);
+    }
+    else {
+      res.status(400).json({ status: true, message: "Error" })
+    }
+  } catch (e) {
+    res.status(400).json({ status: false, message: "Error" + e });
   }
 });
 
 router.post("/add", async function (req, res) {
-    try {
-            const { name, description, timeStart, timeEnd, avatar, images, categories, banner, location, ticketPrice, ticketQuantity, rating } = req.body;
-            const newItem = { name, description, timeStart, timeEnd, avatar, images, categories, banner, location, ticketPrice, ticketQuantity, rating};
-            await eventModel.create(newItem);
-            res.status(200).json({ 
-              status: true,
-              message: "Successfully" 
-            });
-        }
-    catch (e) {
-      res.status(400).json({ status: false, message: "Error" + e});
+  try {
+    const { name, description, timeStart, timeEnd, avatar, images, categories, banner, location, ticketPrice, ticketQuantity, rating } = req.body;
+    const newItem = { name, description, timeStart, timeEnd, avatar, images, categories, banner, location, ticketPrice, ticketQuantity, rating };
+    await eventModel.create(newItem);
+    res.status(200).json({
+      status: true,
+      message: "Successfully"
+    });
+  }
+  catch (e) {
+    res.status(400).json({ status: false, message: "Error" + e });
+  }
+})
+
+router.put("/edit", async function (req, res) {
+  try {
+    const { id, name, description, timeStart, timeEnd, avatar, images, categories, banner, location, ticketPrice, ticketQuantity, rating } = req.body;
+    const itemUpdate = await eventModel.findById(id);
+
+    if (itemUpdate) {
+      itemUpdate.name = name ? name : itemUpdate.name;
+      itemUpdate.description = description ? description : itemUpdate.description;
+      itemUpdate.description = description ? description : itemUpdate.description;
+      itemUpdate.timeStart = timeStart ? timeStart : itemUpdate.timeStart;
+      itemUpdate.timeEnd = timeEnd ? timeEnd : itemUpdate.timeEnd;
+      itemUpdate.avatar = avatar ? avatar : itemUpdate.avatar;
+      itemUpdate.images = images ? images : itemUpdate.images;
+      itemUpdate.categories = categories ? categories : itemUpdate.categories;
+      itemUpdate.banner = banner ? banner : itemUpdate.banner;
+      itemUpdate.ticketPrice = ticketPrice ? ticketPrice : itemUpdate.ticketPrice;
+      itemUpdate.ticketQuantity = ticketQuantity ? ticketQuantity : itemUpdate.ticketQuantity;
+      itemUpdate.location = location ? location : itemUpdate.location;
+      itemUpdate.rating = rating ? rating : itemUpdate.rating;
+
+      await itemUpdate.save();
+      res.status(200).json({ status: true, message: "Successfully" });
     }
+    else {
+      res.status(300).json({ status: true, message: "Not found" });
+    }
+  }
+  catch (e) {
+    res.status(400).json({ status: false, message: "Error" + e });
+  }
 })
 
 module.exports = router;
