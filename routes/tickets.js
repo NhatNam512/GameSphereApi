c8d3ccc7046ecdd820b71ac7a4d4d5d7c244bdd6
Nguyễn Nhật Nam <nhatnam5122004@gmail.com>
2025-03-01 10:37:22 +0700

    Enhance ticket retrieval with user and event details

--- routes/tickets.js
+++ routes/tickets.js
@@ -4,6 +4,7 @@ const JWT = require('jsonwebtoken');
 const config = require("../until/tokenConfig");
 const Ticket = require('../models/ticketModel');
 const Event = require('../models/eventModel')
+const User = require('../models/userModel');
 const mongoose = require('mongoose');
 
 router.get("/all", async function (req, res) {
@@ -22,14 +23,31 @@ router.get("/all", async function (req, res) {
 
 router.get("/getTicket/:userId", async function (req, res) {
     try{
-        const tickets = await Ticket.find({ userId: req.params.userId });
-        if(tickets.length>0){
+        const userId = req.params.userId
+
+        //Lấy thông tin người dùng
+        const user = await User.findOne({userId})
+        if(!user) return res.status(404).json({error: "Not Found User"});
+
+        //Lấy vé của user
+        const tickets = await Ticket.find({ userId: userId });
+        //Lấy danh sách eventId duy nhất
+        const eventIds = [...new Set(tickets.map(t=>t.eventId))];
+        //Lấy thông tin sự kiện 
+        const events = await Event.find({eventId: {$in: eventIds}});
+        //Gộp dữ liệu
+        const result = {
+            user,
+            events: events.map(event=>({
+                ...event,
+                tickets: tickets.filter(t=>t.eventId === event._id),
+            })),
+        }
             res.status(200).json({
               status: true,
               message: "Lấy vé thành công",
-              data: tickets
+              data: result
             })
-          }
     }
     catch(e){
         res.status(404).json({ status: false, message: "Not Found" })
