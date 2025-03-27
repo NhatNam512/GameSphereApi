10e7550ef97dd2cd02525da750760011a3f3e136
Nguyễn Nhật Nam <nhatnam5122004@gmail.com>
2025-03-27 22:31:29 +0700

    fixsocket_4

--- socket/socket.js
+++ socket/socket.js
@@ -1,51 +1,27 @@
 const { Server } = require("socket.io");
 
-let io;
-
 function initializeSocket(server) {
-    io = new Server(server, {
-        cors: { 
-            origin: "*",
-            methods: ["GET", "POST"],
-            credentials: true
-        },
-        transports: ["websocket", "polling"], // Hỗ trợ cả WebSocket và Polling
-        
-    });
-
-    io.on("connection", (socket) => {
-        console.log("Client kết nối:", socket.id);
-
-        // Thêm nhiều log để debug
-        socket.on("connect_error", (error) => {
-            console.error("Socket kết nối lỗi:", error);
-        });
-
-        socket.on("disconnect", (reason) => {
-            console.log("Client ngắt kết nối:", socket.id, "Lý do:", reason);
-        });
-
-        // Ví dụ về việc bắt và log các sự kiện khác
-        socket.on("error", (error) => {
-            console.error("Lỗi socket:", error);
-        });
-    });
-
-    // Thêm log toàn cục cho kết nối
-    io.engine.on("connection_error", (err) => {
-        console.log("Lỗi kết nối socket toàn cục:", err.message);
-        console.log("Mã lỗi:", err.code);
-        console.log("Chi tiết lỗi:", err);
-    });
-
-    return io;
-}
-
-function getSocketIO() {
-    if (!io) {
-        throw new Error("Socket.IO chưa được khởi tạo!");
+  const io = new Server(server, {
+    cors: {
+      origin: "*", // Hoặc chỉ định domain frontend
+      methods: ["GET", "POST"]
     }
-    return io;
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Client đã kết nối:", socket.id);
+
+    socket.on("sendMessage", (data) => {
+      console.log("Tin nhắn nhận được:", data);
+      io.emit("receiveMessage", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Client đã ngắt kết nối:", socket.id);
+    });
+  });
+
+  return io;
 }
 
-module.exports = { initializeSocket, getSocketIO };
+module.exports = { initializeSocket };
