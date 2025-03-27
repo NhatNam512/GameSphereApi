79081057907a5815159a2e9ad2ac5a40eab6916f
Nguyễn Nhật Nam <nhatnam5122004@gmail.com>
2025-03-27 19:37:47 +0700

    fix

--- app.js
+++ app.js
@@ -38,8 +38,7 @@ var previewGameRouter = require('./routes/previewGame');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
-var io = initializeSocket(server);
-
+initializeSocket(server);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
