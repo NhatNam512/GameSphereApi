4da1f441663e51140971573d14bdfe6a1d759dc2
Nguyễn Nhật Nam <nhatnam5122004@gmail.com>
2025-03-27 19:17:24 +0700

    fix

--- routes/previewGame.js
+++ routes/previewGame.js
@@ -13,7 +13,7 @@ router.post("/post", async (req, res) => {
 
         const io = getSocketIO();
         io.emit("new_review", newPost);
-        
+
         res.status(200).json({ status: true, message: "Đăng bài thành công", data: newPost});
     }catch(e){
         res.status(500).json({ status: false, message: "Lỗi server: " + e.message });
@@ -29,6 +29,21 @@ router.get("/all", async (req, res) => {
             data: preview 
           });
     }catch(e){
+        res.status(500).json({ status: false, message: "Lỗi server: " + e.message });
+    }
+});
+
+router.get("/:gameId", async (req, res) => {
+    try {
+        const { gameId } = req.params;
+        const reviews = await previewModel.find({ gameId }).populate("userId");
+
+        res.status(200).json({
+            status: true,
+            message: "Lấy danh sách review thành công",
+            data: reviews
+        });
+    } catch (e) {
         res.status(500).json({ status: false, message: "Lỗi server: " + e.message });
     }
 });
