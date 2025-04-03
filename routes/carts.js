0683d45f4e0dd83504a0a797041cf07dbcd50ff4
Nguyễn Nhật Nam <nhatnam5122004@gmail.com>
2025-04-03 17:56:52 +0700

    Refactor cart addition logic: replaced existing cart update mechanism with direct cart creation for improved efficiency and simplified handling of user orders.

--- routes/carts.js
+++ routes/carts.js
@@ -6,18 +6,10 @@ const cartModel = require('../models/plants/cartModel');
 const plantModel = require('../models/plants/plantModel');
 
 router.post("/add", async (req, res) => {
-    const { userId, products, status } = req.body;
     try {
-      let cart = await cartModel.findOne({ userId });
-      if (!cart) {
-        cart = new cartModel({ userId, items: [], status: status });
-      }
-      for (const { productId, quantity } of products) {
-        const product = await plantModel.findById(productId);
-        if (!product) return res.status(404).json({ message: `Sản phẩm ${productId} không tồn tại` });
-        cart.items.push({ productId: productId, quantity, price: product.price });
-      }
-      await cart.save();
+      const { userId, products, status } = req.body;
+      const newOrder =  { userId, products, status }
+      await cartModel.create(newOrder);
       res.status(200).json({
         status: true,
         message: "Thêm giỏ hàng thành công",
