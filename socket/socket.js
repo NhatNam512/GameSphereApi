c94c03365e474b4967cddb8646a5408dba4357fa
Nguyễn Nhật Nam <nhatnam5122004@gmail.com>
2025-04-09 15:41:15 +0700

    Add socket message handling: implemented message event listener for broadcasting messages to all clients and added functions for sending messages to specific clients.

--- socket/socket.js
+++ socket/socket.js
@@ -9,7 +9,15 @@ function initializeSocket(server) {
 
     io.on("connection", (socket) => {
         console.log("Client kết nối:", socket.id);
-
+        socket.on("message", (data) => {
+            console.log(`Tin nhắn từ ${socket.id}:`, data);
+            
+            // Gửi tin nhắn cho tất cả clients khác
+            socket.broadcast.emit("message", data);
+            
+            // Hoặc có thể xử lý và phản hồi cho client gửi
+            // socket.emit("message", `Server đã nhận: ${data}`);
+        });
         socket.on("disconnect", () => {
             console.log("Client ngắt kết nối:", socket.id);
         });
@@ -18,11 +26,28 @@ function initializeSocket(server) {
     return io;
 }
 
-function getSocketIO() {
+// Hàm gửi tin nhắn tới tất cả clients
+function broadcastMessage(event, data) {
     if (!io) {
         throw new Error("Socket.IO chưa được khởi tạo!");
     }
-    return io;
+    io.emit(event, data);
 }
 
-module.exports = { initializeSocket, getSocketIO };
+// Hàm gửi tin nhắn tới một client cụ thể
+function sendToClient(socketId, event, data) {
+    if (!io) {
+        throw new Error("Socket.IO chưa được khởi tạo!");
+    }
+    const socket = io.sockets.sockets.get(socketId);
+    if (socket) {
+        socket.emit(event, data);
+    }
+}
+
+module.exports = { 
+    initializeSocket, 
+    getSocketIO,
+    broadcastMessage,
+    sendToClient
+};
