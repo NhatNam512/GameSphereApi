c30621e4e85affb50342b764b0148cbb6d01d880
Nguyễn Nhật Nam <nhatnam5122004@gmail.com>
2025-04-09 18:35:24 +0700

    Remove server listening logic from app.js to streamline the application structure and focus on middleware functionality.

--- app.js
+++ app.js
@@ -118,9 +118,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 module.exports = app;
