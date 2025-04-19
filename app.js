11b51af6e12e59b883ce3017d3f099d83cc36f2e
Nguyễn Nhật Nam <nhatnam5122004@gmail.com>
2025-04-19 11:48:25 +0700

    update v2

--- app.js
+++ app.js
@@ -5,37 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 const compression = require('compression');
+const connectDB = require('./src/config/database');
+const errorHandler = require('./src/middlewares/errorHandler');
 
-const mongoose = require('mongoose');
-
-require("./models/userModel");
-require("./models/events/categoryModel");
-require("./models/events/eventModel");
-require("./models/events/orderModel");
-require("./models/events/ticketModel");
-require("./models/events/previewEventModel");
-require("./models/games/gameModel");
-require("./models/games/previewGameModel");
-require("./models/games/categoriesGameModel");
-require("./models/games/previewGameModel");
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var categoriesRouter = require('./routes/categories');
-var eventsRouter = require('./routes/events');
-var ordersRouter = require('./routes/orders');
-var ticketsRouter = require('./routes/tickets');
-var paymentRouter = require("./routes/payments");
-var emailRouter = require("./routes/emails");
-var gamesRouter = require('./routes/games');
-var recommendRouter = require("./routes/recommendation");
-var categoriesGamesRouter = require('./routes/categoriesGames');
-var previewGameRouter = require('./routes/previewGame');
-var previewEventRouter = require('./routes/previewEvent');
+var router = require('./src/routes')
 
 var app = express();
 var http = require('http');
-var server = http.createServer(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,37 +24,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-//connect database
-mongoose.connect('mongodb+srv://namnnps38713:wcVNA8PAeuqTioxq@namnnps38713.bctmi.mongodb.net/gamesphere')
+
+// Kết nối database
+connectDB()
   .then(() => console.log('>>>>>>>>>> DB Connected!!!!!!'))
   .catch(err => console.log('>>>>>>>>> DB Error: ', err));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/games', gamesRouter);
-app.use('/categories', categoriesRouter);
-app.use('/events', eventsRouter);
-app.use('/orders', ordersRouter);
-app.use('/tickets', ticketsRouter);
-app.use("/payments", paymentRouter);
-app.use("/emails", emailRouter);
-app.use("/recommend", recommendRouter);
-app.use("/categories_games", categoriesGamesRouter);
-app.use("/previewGame", previewGameRouter);
-app.use("/previewEvent", previewEventRouter);
+app.use('/api', router);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// Sử dụng error handler middleware
+app.use(errorHandler);
 
 module.exports = app;
