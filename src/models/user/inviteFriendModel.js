6a6d01ec7b19a0df595fd3e1c0c952b47029ae8a
NhatNam512 <nhatnam5122004@gmail.com>
2025-05-10 19:21:08 +0700

    Fix model export in inviteFriendModel: updated the export statement to correctly reference inviteFriendSchema instead of friendshipSchema, ensuring proper model functionality.

--- src/models/user/inviteFriendModel.js
+++ src/models/user/inviteFriendModel.js
@@ -11,4 +11,4 @@ const inviteFriendSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.models.friendships || mongoose.model('friendships', friendshipSchema);
+module.exports = mongoose.models.friendships || mongoose.model('friendships', inviteFriendSchema);
